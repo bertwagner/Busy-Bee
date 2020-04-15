eb8bd5ca1e42070589314af35ad929355f07cfc7
Bert Wagner <github@bertwagner.com>
2020-04-14 20:38:34 -0400

    simplifies script

--- .github/actions/deploy-s3/entrypoint.sh
+++ .github/actions/deploy-s3/entrypoint.sh
@@ -1,22 +1,23 @@
 #!/bin/sh
 
-set -e
+echo "hi!"
+# set -e
 
-aws configure --profile deploy-s3 <<-EOF > /dev/null 2>&1
-${AWS_ACCESS_KEY_ID}
-${AWS_SECRET_ACCESS_KEY}
-${AWS_REGION}
-text
-EOF
+# aws configure --profile deploy-s3 <<-EOF > /dev/null 2>&1
+# ${AWS_ACCESS_KEY_ID}
+# ${AWS_SECRET_ACCESS_KEY}
+# ${AWS_REGION}
+# text
+# EOF
 
-sh -c "aws s3 sync ${SOURCE_DIR:-.} s3://${AWS_S3_BUCKET} \
-              --profile deploy-s3 \
-              --no-progress \
-              $*"
+# sh -c "aws s3 sync ${SOURCE_DIR:-.} s3://${AWS_S3_BUCKET} \
+#               --profile deploy-s3 \
+#               --no-progress \
+#               $*"
 
-aws configure --profile deploy-s3 <<-EOF > /dev/null 2>&1
-null
-null
-null
-text
-EOF
+# aws configure --profile deploy-s3 <<-EOF > /dev/null 2>&1
+# null
+# null
+# null
+# text
+# EOF
