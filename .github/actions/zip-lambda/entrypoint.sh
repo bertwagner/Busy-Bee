a54cb7a873cf5f14359b48bc06e1188b6636c37d
Bert Wagner <github@bertwagner.com>
2020-04-19 14:09:17 -0400

    autocomplete screwed me over on taht last one.

--- .github/actions/zip-lambda/entrypoint.sh
+++ .github/actions/zip-lambda/entrypoint.sh
@@ -16,7 +16,7 @@ install_dependencies() {
 
 zip_files() {
 	echo "Copying Lambda scripts into depedencies folder"
-	cp *.publish_dependencies_as_layer /python
+	cp *.py /python
 	Echo "Zipping everything up"
 	zip -r lambda_funcion.zip ./python
 }
@@ -25,25 +25,6 @@ deploy_to_lambda() {
 	echo "Deploying the code to AWS Lambda"
 	aws lambda update-function-code --function-name "${LAMBDA_NAME}" --zip-file fileb://lambda_function.zip
 }
-
-# # publish_dependencies_as_layer(){
-# # 	echo "Publishing dependencies as a layer..."
-# # 	local result=$(aws lambda publish-layer-version --layer-name "${INPUT_LAMBDA_LAYER_ARN}" --zip-file fileb://dependencies.zip)
-# # 	LAYER_VERSION=$(jq '.Version' <<< "$result")
-# # 	rm -rf python
-# # 	rm dependencies.zip
-# # }
-
-# # publish_function_code(){
-# # 	echo "Deploying the code itself..."
-# # 	zip -r code.zip . -x \*.git\*
-# # 	aws lambda update-function-code --function-name "${INPUT_LAMBDA_FUNCTION_NAME}" --zip-file fileb://code.zip
-# # }
-
-# # update_function_layers(){
-# # 	echo "Using the layer in the function..."
-# # 	aws lambda update-function-configuration --function-name "${INPUT_LAMBDA_FUNCTION_NAME}" --layers "${INPUT_LAMBDA_LAYER_ARN}:${LAYER_VERSION}"
-# # }
 
 deploy_lambda_function() {
     configure_aws_credentials
