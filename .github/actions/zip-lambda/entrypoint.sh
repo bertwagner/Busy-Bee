24256c256d2b423b13b1976c8b9e4f6e42d09459
Bert Wagner <github@bertwagner.com>
2020-04-19 13:38:16 -0400

    reconsidering how i'm doing this

--- .github/actions/zip-lambda/entrypoint.sh
+++ .github/actions/zip-lambda/entrypoint.sh
@@ -1,49 +1,53 @@
-#!/bin/bash
+#!/bin/sh 
 
 set -e
+echo "BERT WAS HERE"
+# #!/bin/bash
 
-configure_aws_credentials(){
-	aws configure set aws_access_key_id "${AWS_ACCESS_KEY_ID}"
-    aws configure set aws_secret_access_key "${AWS_SECRET_ACCESS_KEY}"
-    aws configure set default.region "${AWS_REGION}"
-}
+# set -e
 
-# install_zip_dependencies(){
-# 	echo "Installing and zipping dependencies..."
-# 	mkdir python
-#     echo "Looking into Directory: ${LAMBDA_DIRECTORY}"
-# 	pip install --target=python -r "${LAMBDA_DIRECTORY}/requirements.txt"
-# 	zip -r dependencies.zip ./python
+# configure_aws_credentials(){
+# 	aws configure set aws_access_key_id "${AWS_ACCESS_KEY_ID}"
+#     aws configure set aws_secret_access_key "${AWS_SECRET_ACCESS_KEY}"
+#     aws configure set default.region "${AWS_REGION}"
 # }
 
-# publish_dependencies_as_layer(){
-# 	echo "Publishing dependencies as a layer..."
-# 	local result=$(aws lambda publish-layer-version --layer-name "${INPUT_LAMBDA_LAYER_ARN}" --zip-file fileb://dependencies.zip)
-# 	LAYER_VERSION=$(jq '.Version' <<< "$result")
-# 	rm -rf python
-# 	rm dependencies.zip
+# # install_zip_dependencies(){
+# # 	echo "Installing and zipping dependencies..."
+# # 	mkdir python
+# #     echo "Looking into Directory: ${LAMBDA_DIRECTORY}"
+# # 	pip install --target=python -r "${LAMBDA_DIRECTORY}/requirements.txt"
+# # 	zip -r dependencies.zip ./python
+# # }
+
+# # publish_dependencies_as_layer(){
+# # 	echo "Publishing dependencies as a layer..."
+# # 	local result=$(aws lambda publish-layer-version --layer-name "${INPUT_LAMBDA_LAYER_ARN}" --zip-file fileb://dependencies.zip)
+# # 	LAYER_VERSION=$(jq '.Version' <<< "$result")
+# # 	rm -rf python
+# # 	rm dependencies.zip
+# # }
+
+# # publish_function_code(){
+# # 	echo "Deploying the code itself..."
+# # 	zip -r code.zip . -x \*.git\*
+# # 	aws lambda update-function-code --function-name "${INPUT_LAMBDA_FUNCTION_NAME}" --zip-file fileb://code.zip
+# # }
+
+# # update_function_layers(){
+# # 	echo "Using the layer in the function..."
+# # 	aws lambda update-function-configuration --function-name "${INPUT_LAMBDA_FUNCTION_NAME}" --layers "${INPUT_LAMBDA_LAYER_ARN}:${LAYER_VERSION}"
+# # }
+
+# deploy_lambda_function(){
+#     configure_aws_credentials
+# 	# install_zip_dependencies
+# 	# publish_dependencies_as_layer
+# 	# publish_function_code
+# 	# update_function_layers
 # }
 
-# publish_function_code(){
-# 	echo "Deploying the code itself..."
-# 	zip -r code.zip . -x \*.git\*
-# 	aws lambda update-function-code --function-name "${INPUT_LAMBDA_FUNCTION_NAME}" --zip-file fileb://code.zip
-# }
-
-# update_function_layers(){
-# 	echo "Using the layer in the function..."
-# 	aws lambda update-function-configuration --function-name "${INPUT_LAMBDA_FUNCTION_NAME}" --layers "${INPUT_LAMBDA_LAYER_ARN}:${LAYER_VERSION}"
-# }
-
-deploy_lambda_function(){
-    configure_aws_credentials
-	# install_zip_dependencies
-	# publish_dependencies_as_layer
-	# publish_function_code
-	# update_function_layers
-}
-
-echo "test bert1234"
-printf "TEST TEST BERT1234 TEST"
-deploy_lambda_function
-echo "Each step completed, check the logs if any error occured."
+# echo "test bert1234"
+# printf "TEST TEST BERT1234 TEST"
+# deploy_lambda_function
+# echo "Each step completed, check the logs if any error occured."
