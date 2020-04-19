88d9a29e7b3b5c41b0d7d88dd959b4bc58b60a7f
Bert Wagner <github@bertwagner.com>
2020-04-19 14:16:32 -0400

    get rid of *

--- .github/actions/zip-lambda/entrypoint.sh
+++ .github/actions/zip-lambda/entrypoint.sh
@@ -16,7 +16,7 @@ install_dependencies() {
 
 zip_files() {
 	echo "Copying Lambda scripts into depedencies folder"
-	cp -R "Lambda/${LAMBDA_NAME}/*" python/
+	cp -R "Lambda/${LAMBDA_NAME}/" python/
 	Echo "Zipping everything up"
 	zip -r lambda_funcion.zip ./python
 }
